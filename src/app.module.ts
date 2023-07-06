cb187f93f44e13a84d448934b9d124e10ef67183
fffk10 <warafffk10@gmail.com>
2023-07-07 02:00:31 +0900

    corst対応

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AppDataSource } from './data-source'
 import { BooksController } from '@/src/controller/books.controller'
 import { Book } from '@/src/entity/books.entity'
+import { CorsMiddleware } from '@/src/cors.middleware'
 
 @Module({
   imports: [
@@ -14,4 +15,8 @@ import { Book } from '@/src/entity/books.entity'
   controllers: [AppController, BooksController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorsMiddleware).forRoutes('*')
+  }
+}
